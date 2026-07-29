90c4d34821e042352094b9fb45ab73db8266157f
khushwantdangi25 <khushwantdangi25@gmail.com>
2026-07-29 21:49:07 +0530

    bank

--- constructor/script.js
+++ constructor/script.js
@@ -1,28 +1,70 @@
-function BankAccount( customername ,balance =0  ){
-    this.customername = customername
-    this.balance = balance
-    this.accountno = Date.now()
-
-    this.deposit = function(amount){
-        this.balance += amount;
-    } 
-
-    this.withdrawl = function(amount){
-        this.balance -= amount
-    }
-    
+function BankAccount(customername, balance = 0) {
+  this.customername = customername;
+  this.balance = balance;
+  this.accountno = Date.now();
 }
 
+BankAccount.prototype.deposit = function(amount){
+        this.balance += amount;
+  };
+BankAccount.prototype.withdrawl = function(amount){
+        this.balance -= amount;
+  };
+
+
+const user = [];
 const acc = document.querySelector("#accountForm");
 const name = document.querySelector("#customerName");
 const balance = document.querySelector("#balance");
-acc.addEventListener("click" , function(e){
-    e.preventDefault();
-    
-})
 
-// const Ram  = new BankAccount("Ram" , 1000);
-// const Sham  = new BankAccount("Sham" , 1000);
-// Sham.deposit(100);
-// Sham.withdrawl(200);
-// console.log(Ram , Sham);
+acc.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const bank = new BankAccount(name.value, +balance.value);
+  const bank2 = new BankAccount(name.value, +balance.value);
+
+  user.push(bank);
+  user.push(bank2);
+
+  console.log(user);
+});
+
+//deposite working
+const limitDepo = 100000;
+const depo = document.querySelector("#depositForm");
+const accNum = document.querySelector("#accNum");
+const amount = document.querySelector("#amnt");
+
+depo.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const account = user.find((u) => u.accountno === +accNum.value);
+  if(+amount.value <= limitDepo){
+    account.deposit(+amount.value);
+    
+    console.log(account);
+  }
+  else{
+    alert("Aukat ke bhar ")
+  }
+  
+});
+
+//withdrawl working
+
+
+const withdraw = document.querySelector("#withdraw");
+const accountNum = document.querySelector("#accountNumber");
+const withamount = document.querySelector("#amount");
+
+withdraw.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const account = user.find((u) => u.accountno === +accountNum.value);
+  if(+withamount.value <= limitDepo ){
+         account.withdrawl(+withamount.value);
+       console.log(account);
+  }
+  else{
+    alert("Nhi hoga")
+  }
+ 
+});
